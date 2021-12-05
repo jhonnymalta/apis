7e69e824db249fb6508c29905fe93c7a9b771467
Jota Landes <jhonatamalta@hotmail.com>
2021-12-05 16:24:01 -0300

    insert create-user-token and autenticate login system

--- 002-autenticacao/controllers/userController.js
+++ 002-autenticacao/controllers/userController.js
@@ -2,7 +2,8 @@
 const userModel = require("../models/User");
 
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+
+const createUserToken = require("../helpers/create-user-token");
 
 module.exports = class userController {
   static async newUser(req, res) {
@@ -39,17 +40,61 @@ module.exports = class userController {
     }
     const passwordHash = await bcrypt.hash(password, 8);
 
-    const newUser = new User({
+    const newUser = new userModel({
       name: name,
       email: email,
       password: passwordHash,
     });
     try {
-      newUser.save({ newUser });
-      res.status(201).json({ message: "Usuário criado" });
-      console.log(newUser);
+      await newUser.save();
+
+      await createUserToken(newUser, req, res);
     } catch (error) {
-      res.status(422).json({ message: "Erro ao cadastrar usuário" });
+      res.status(500).json({ message: error });
     }
+  }
+
+  static async deleteUser(req, res) {
+    const usuario = req.params.id;
+
+    try {
+      await userModel.findOneAndDelete({ _id: usuario });
+      res.status(200).json({
+        message: "Usuárido deletado com sucesso!",
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+
+  static async userLogin(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "Email obrigatório." });
+      return;
+    }
+    if (!password) {
+      res.status(422).json({ message: "password obrigatório." });
+      return;
+    }
+
+    //check if email exist
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.status(422).json({ message: "Email não cadastrado!" });
+      return;
+    }
+
+    // check if password match wit db
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: "Senha Inválida" });
+      return;
+    }
+
+    await createUserToken(user, req, res);
   }
 };
--- 002-autenticacao/routes/userRoutes.js
+++ 002-autenticacao/routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 //controller
 const userController = require("../controllers/userController");
 
-router.post("/", userController.newUser);
+router.post("/register", userController.newUser);
+router.post("/login", userController.userLogin);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
